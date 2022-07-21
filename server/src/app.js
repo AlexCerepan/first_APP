bbfcc331586305d4d82a0f22a3672fdef3cbbd48
MatusMakay <makay2596@gmail.com>
2022-07-21 17:09:37 +0200

    added documentation dir

--- server/src/app.js
+++ server/src/app.js
@@ -60,3 +60,30 @@ app.get('/*', (req, res) => {
  * EXPORT
  */
 module.exports = app
+ 
+/**
+ * alex app
+ * 
+ * 
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const serverLogger = require("easy-log")("app:server", { colorCode: 200 });
+const cors = require("cors");
+var app = express();
+
+app.use((req, res, next) => {
+  serverLogger("${req.method} - ${req.originalUrl}");
+  next();
+});
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(cors());
+
+app.get("/", (req, res, next) => {
+  res.send("hello world");
+});
+
+ * 
+ */
